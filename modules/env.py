0cbacbd97bda7f46e0903f2483b38bf567b94eb2
Peter Weston <31358773+pawkw@users.noreply.github.com>
2019-10-16 21:00:18 -0400

    Work on environment.

--- modules/env.py
+++ modules/env.py
@@ -1,23 +1,85 @@
 # Environment class
 import pwl_types as types
 
-class Environment:
-    def __init__(self, parent = None, args = none, values = none):
-        self.defs = {}
+class Environment(dict):
+    def __init__(self, parent = None, args = None, values = None):
+        '''
+        Create a new environment.
+
+        parent is the parent environment.
+        args a list of arguments.
+        values is a list of values.
+
+        For variadic args(like *args) the '.' token is used.
+
+        >>> e = Environment(None, ['a', '.', 'b'], [1, 2, 3])
+        >>> e['a']
+        1
+        >>> e['b']
+        <PWLisp list (2 3)>
+        '''
+
         self.parent = parent
+        self.args = args
+        self.values = values
 
         # Bind arguments to values
-        # Handle variadic binding
+        if args:
+            while args:
+                arg = args.pop(0)
+                value = values.pop(0)
+                # Handle variadic
+                if arg == '.':
+                    arg = args.pop(0)
+                    values = [value]+values
+                    self[arg] = types.List(values)
+                    break
+                self[arg] = value
 
     def __str__(self):
-        pass
+        result = ""
+        for item in self.keys():
+            result += str(item).rjust(15)+" : "+str(self[item])
+
+    def __repr__(self):
+        result = " ".join(self.keys())
+        return '<PWLisp environment : {}>'.format(result)
 
     # TODO: Look up how to implement dict-like functions
     def set(self, key, value):
-        pass
+        self[key] = value
 
     def find(self, key):
-        pass
+        '''
+        Find a key in this environment or its parent.
+
+        >>> e = Environment(None, ['a', 'b'], [1, 2])
+        >>> e['b']
+        2
+        >>> e.find('a')
+        <PWLisp environment : a b>
+        >>> e.find('Dave') # None is returned
+        '''
+        if key in self:
+            return self
+        if self.parent:
+            return self.parent.find(key)
 
     def get(self, key):
-        pass
+        '''
+        Get the value of a key in this environment or its parent.
+
+        >>> parent = Environment(None, ['a', 'c'], ['Oh oh!', 3])
+        >>> e = Environment(parent, ['a', 'b'], [1, 2])
+        >>> e.get('a')
+        1
+        >>> e.get('b')
+        2
+        >>> e.get('c')
+        3
+        '''
+
+        env = self.find(key)
+        if env:
+            return env[key]
+        raise Exception("Key {} was not found in environment.")
